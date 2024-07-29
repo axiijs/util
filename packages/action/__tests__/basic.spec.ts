0d8f9991ab0208b7be7765c55a0f0ab80d5b6698
sskyy <skyking_H@hotmail.com>
2024-07-30 00:38:04 +0800

    feat: action complete

---
+++ packages/action/__tests__/basic.spec.ts
@@ -0,0 +1,130 @@
+/** @vitest-environment jsdom */
+/** @jsx createElement */
+import {Action, SerialAction, STATUS_ABORT, STATUS_PENDING, STATUS_PROCESSING, STATUS_SUCCESS} from "../src/action";
+import {beforeEach, expect, describe, test} from "vitest";
+import {atom, Atom, RxList} from "data0";
+
+function wait(time: number) {
+    return new Promise(resolve => {
+        setTimeout(resolve, time)
+    })
+}
+
+describe('parallel', () => {
+    let parallel!: Action<any>
+
+    beforeEach(() => {
+        parallel = new Action(async (...args: any[]) => {
+            await wait(200)
+            return args
+        }, {
+            parallelLimit: 3
+        })
+    })
+
+
+    test('basic', async () => {
+        const list = new RxList<{id:number, status:Atom<number>}>([])
+        const pendingList = list.filter(item => item.status() === STATUS_PENDING)
+        pendingList.length()
+        const processingList = list.filter(item => item.status() === STATUS_PROCESSING)
+        processingList.length()
+        list.push({id:1, status:atom(STATUS_PENDING)})
+
+        expect(pendingList.length.raw).toBe(1)
+        expect(processingList.length.raw).toBe(0)
+
+        pendingList.data.at(0)!.status(STATUS_PROCESSING)
+        expect(pendingList.length.raw).toBe(0)
+        expect(processingList.length.raw).toBe(1)
+
+        processingList.data.at(0)!.status(STATUS_SUCCESS)
+        expect(pendingList.length.raw).toBe(0)
+        expect(processingList.length.raw).toBe(0)
+    })
+
+    test('parallel limit', async () => {
+        const p1 = parallel.run(1,2)
+        const p2 = parallel.run(2,3)
+        const p3 = parallel.run(3,4)
+
+        await wait(100)
+        expect(p1.status.raw).toBe(STATUS_PROCESSING)
+        expect(p2.status.raw).toBe(STATUS_PROCESSING)
+        expect(p3.status.raw).toBe(STATUS_PROCESSING)
+
+        await wait(200)
+        expect(p1.status.raw).toBe(STATUS_SUCCESS)
+        expect(p2.status.raw).toBe(STATUS_SUCCESS)
+        expect(p3.status.raw).toBe(STATUS_SUCCESS)
+        expect(p1.data.raw).toMatchObject([1,2])
+        expect(p2.data.raw).toMatchObject([2,3])
+        expect(p3.data.raw).toMatchObject([3,4])
+    })
+
+    test('beyond parallel limit should abort oldest', async () => {
+        const p1 = parallel.run(1,2)
+        const p2 = parallel.run(2,3)
+        const p3 = parallel.run(3,4)
+
+        await wait(10)
+        expect(p1.status.raw).toBe(STATUS_PROCESSING)
+
+        const p4 = parallel.run(4,5)
+        expect(p1.status.raw).toBe(STATUS_ABORT)
+        expect(p2.status.raw).toBe(STATUS_PROCESSING)
+        expect(p3.status.raw).toBe(STATUS_PROCESSING)
+        await wait(10)
+        expect(p4.status.raw).toBe(STATUS_PROCESSING)
+
+        await wait(200)
+        expect(p2.status.raw).toBe(STATUS_SUCCESS)
+        expect(p3.status.raw).toBe(STATUS_SUCCESS)
+        expect(p4.status.raw).toBe(STATUS_SUCCESS)
+    })
+})
+
+describe('serial', () => {
+    let serial!: SerialAction<any>
+
+    beforeEach(() => {
+        serial = new SerialAction(async (...args: any[]) => {
+            await wait(100)
+            return args
+        }, {})
+    })
+
+    test('serial limit', async () => {
+        const p1 = serial.run(1,2)
+        const p2 = serial.run(2,3)
+        const p3 = serial.run(3,4)
+
+        await wait(10)
+        expect(p1.status.raw).toBe(STATUS_PROCESSING)
+        expect(p2.status.raw).toBe(STATUS_PENDING)
+        expect(p3.status.raw).toBe(STATUS_PENDING)
+
+        await p1.resolvers.promise
+        expect(p1.status.raw).toBe(STATUS_SUCCESS)
+
+        expect(serial.pendingProcesses.length.raw).toBe(2)
+        expect(serial.completedProcesses.length.raw).toBe(1)
+        expect(serial.processingProcesses.length.raw).toBe(0)
+
+        await wait(10)
+        expect(p1.status.raw).toBe(STATUS_SUCCESS)
+        await wait(10)
+
+        expect(p2.status.raw).toBe(STATUS_PROCESSING)
+        expect(p3.status.raw).toBe(STATUS_PENDING)
+
+        await p2.resolvers.promise
+        await wait(10)
+        expect(p2.status.raw).toBe(STATUS_SUCCESS)
+        expect(p3.status.raw).toBe(STATUS_PROCESSING)
+
+        await p3.resolvers.promise
+        await wait(10)
+        expect(p3.status.raw).toBe(STATUS_SUCCESS)
+    })
+})
---
+++ packages/action/src/action.ts
@@ -0,0 +1,177 @@
+
+// 控制能力
+//  1. 同时能有多少个
+//  2. 超出限制的时候，创建新的时候是等待还是覆盖？
+//  3. 覆盖的哪一个？
+
+// 默认都可以取消，用户在 UI 上去做限制就行了。
+import {atom, Atom, computed, once, RxList} from "data0";
+
+
+export const STATUS_PENDING= -1
+export const STATUS_PROCESSING= 1
+export const STATUS_SUCCESS= 2
+export const STATUS_ERROR= 3
+export const STATUS_ABORT= 4
+export type STATUS_TYPE = typeof STATUS_PENDING |typeof STATUS_PROCESSING | typeof STATUS_SUCCESS | typeof STATUS_ERROR | typeof STATUS_ABORT
+
+
+export type RunFN = (...args:any[]) => any
+
+
+function withResolvers() {
+    let resolve:(data:any)=>void
+    let reject:(error:any)=>void
+    const promise = new Promise((res, rej) => {
+        resolve = res
+        reject = rej
+    })
+    // @ts-ignore
+    return {promise, resolve, reject}
+}
+
+
+export class ActionProcess<T extends RunFN> {
+    static id = 1
+    public status: Atom<STATUS_TYPE> = atom(STATUS_PENDING)
+    public data: Atom<any> = atom(null)
+    public error: Atom<any> = atom(null)
+    // @ts-ignore
+    public resolvers = withResolvers() as {promise:Promise<any>, resolve:()=>void, reject:()=>void}
+    public id: number
+    constructor(public fn:T, public args: Parameters<T>) {
+        this.id = ActionProcess.id++
+    }
+    start = async () => {
+        if (this.status.raw !== STATUS_PENDING) {
+            throw new Error('ActionProcess is already started')
+        }
+        this.status(STATUS_PROCESSING)
+        try {
+            const data = await this.fn(...this.args)
+            // @ts-ignore
+            if (this.status.raw !== STATUS_ABORT) {
+                this.data(data)
+                this.status(STATUS_SUCCESS)
+            }
+        } catch (e) {
+            this.error(e)
+            this.status(STATUS_ERROR)
+        }
+        this.resolvers.resolve()
+    }
+    abort() {
+        if(this.status.raw === STATUS_PENDING || this.status.raw === STATUS_PROCESSING) {
+            this.status(STATUS_ABORT)
+        }
+    }
+}
+
+type ActionClassOptions = {
+    // parallel limit, 1 的时候就是串行
+    parallelLimit?: number
+    pending?: {
+        replace?: boolean,
+        replaceOldest?: boolean
+    }
+    // 保存历史
+    historyLimit? :number,
+
+}
+
+export class Action<T extends RunFN> {
+    public processes: RxList<ActionProcess<T>> = new RxList<ActionProcess<T>>([])
+    public pendingProcesses: RxList<ActionProcess<T>> = this.processes.filter(p => {
+        return p.status() === STATUS_PENDING
+    })
+    public processingProcesses: RxList<ActionProcess<T>> = this.processes.filter(p => {
+        return p.status() === STATUS_PROCESSING
+    })
+    public completedProcesses: RxList<ActionProcess<T>> = this.processes.filter(p => p.status() > STATUS_PROCESSING)
+    resolvers?: {promise:Promise<any>, resolve:()=>void, reject:()=>void}
+    public promise: Atom<Promise<any>|null> = atom(null)
+    constructor(public fn: T, public options: ActionClassOptions = {}) {
+
+    }
+    run = (...args: Parameters<T>) => {
+        const process = new ActionProcess<T>(this.fn, args)
+
+        const {
+            parallelLimit = Infinity,
+            pending = { replace: true, replaceOldest: true},
+            historyLimit = 0
+        } = this.options
+
+        // 超过了能同时处理的数量限制，如果是 replace 就要把之前的 abort 掉
+        if (this.processingProcesses.data.length >= parallelLimit) {
+            if (pending.replace) {
+                const toReplace = this.processingProcesses.data.at(pending.replaceOldest? 0 : -1)!
+                toReplace.abort()
+            }
+        }
+
+        // 删掉已经完成的，从头删除超过 historyLimit 的
+        if (this.completedProcesses.data.length > historyLimit) {
+            this.processes.splice(0, this.completedProcesses.data.length - historyLimit)
+        }
+
+        this.processes.push(process)
+        if (!this.promise.raw) {
+            this.start()
+        }
+        return process
+    }
+    start = async () => {
+        if(this.promise.raw) {
+            return this.promise.raw
+        }
+
+        const {promise, resolve} = withResolvers()
+        this.promise(promise)
+
+        const {parallelLimit = Infinity} = this.options
+
+        once(() => {
+            // watch pendingProcesses & dispose length change
+            if (this.pendingProcesses.length()) {
+                if(this.processingProcesses.length() < parallelLimit) {
+                    const process = this.pendingProcesses.data.at(0)!
+                    // CAUTION 一定要放到下个 tick，不然会这里就会立刻触发上面的 length 变化，然后 once 又触发。
+                    //  data0逻辑里面已经 recomputing 的 computed 遇到重算会认为是依赖脏了，于是直接重算，不会再次 schedule。
+                    Promise.resolve().then(() => process.start())
+                }
+            } else {
+                if (!this.processingProcesses.length()) {
+                    resolve(null)
+                    this.promise(null)
+                    // 撤销 once
+                    return true
+                }
+            }
+        }, (rerun) => {
+            // CAUTION 确保 processingProcesses 和 pendingProcesses 的都变化完了，不然 once 会卡在中间状态触发
+            setTimeout(() => {
+                rerun()
+            }, 1)
+        })
+
+        return promise
+
+    }
+    latest() {
+        return computed(() => this.processes.at(0))
+    }
+}
+
+export class SerialAction<T extends (...args:any[]) => any> extends Action<T>{
+    constructor(public fn: T, options:Omit<ActionClassOptions, 'parallelLimit'>) {
+        super(fn, {...options, parallelLimit: 1, pending: { replace: false}})
+    }
+}
+
+export class SingleAction<T extends (...args:any[]) => any> extends Action<T>{
+    constructor(public fn: T, options:Omit<ActionClassOptions, 'parallelLimit'>) {
+        super(fn, {...options, parallelLimit: 1, pending: { replace: true}})
+    }
+}
+
